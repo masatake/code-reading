439f17ce124fa7be6ae6614d66f5031424344b99
Masatake YAMATO <yamato@redhat.com>
2015-11-04 01:01:43 +0900

    Write about the usage of ctags partially

---
+++ slides/12ctags-usage.s/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+static boolean debug;
+#include DEBUG(X) (debug=X)
+
+struct point2d {int x, y;};
+typedef struct point2d *pp;
+
+int distance (pp p0, pp p1) {   /* ... */}
+int main (int argc, char** argv) {
+
+  int local;
+/* ... */
+ goto_label:
+  return 0;
+}
